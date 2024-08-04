f2b42dfd1d9754c641b4d10947b351e0e4903d58
Ketan <ketansingharoy@gmail.com>
2024-08-04 18:58:29 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,20 +12,20 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    # 'sphinx.ext.duration',
-    # 'sphinx.ext.doctest',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',          # To generate autodocs
-    # 'sphinx.ext.autosummary',
-    # 'sphinx.ext.intersphinx',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',         # For auto-doc configuration
-    # 'sphinx.ext.viewcode',
+    'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',          # autodoc with maths
     'sphinx_copybutton',
 ]
 napoleon_google_docstring = False   # Turn off googledoc strings
 napoleon_numpy_docstring = True     # Turn on numpydoc strings
 napoleon_use_ivar = True 	     # For maths symbology
-# autosummary_generate = True
+autosummary_generate = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
