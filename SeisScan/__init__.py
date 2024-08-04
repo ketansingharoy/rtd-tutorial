43a67b1462ffd8a76b34b69e43360a1bf17b7e5b
Ketan <ketansingharoy@gmail.com>
2024-08-04 02:46:41 -0500

    Update __init__.py

--- SeisScan/__init__.py
+++ SeisScan/__init__.py
@@ -5,5 +5,6 @@ SeisScan - Python library for microearthquake detection and location.
 __version__ = "0.1.0"
 
 from .read_fdsn import read_fdsn, read_fdsn_inventory
+from .read_data.read_example import read_example
 
-__all__ = ['read_fdsn', 'read_fdsn_inventory']
+__all__ = ['read_fdsn', 'read_fdsn_inventory', 'read_example']
