3badd9f16e9c124e17b7abb6a921f698d9a54aeb
Ketan <ketansingharoy@gmail.com>
2024-08-05 14:49:31 -0500

    Update read_example.py

--- SeisScan/read_data/read_example.py
+++ SeisScan/read_data/read_example.py
@@ -1,6 +1,8 @@
 import os
 import json
 import pickle
+from obspy import read_inventory
+from obspy.taup import taup_create
 
 def read_example():
     """
@@ -11,10 +13,18 @@ def read_example():
     #--- example data directory
     data_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
     data_dir = os.path.realpath(data_dir)
+    
+    #--- read event information
+    event_file = os.path.join(data_dir, "evt_dict.json")
+    event_dict = json.load(open(event_file, "r"))
 
     #--- load data_file that contains obspy stream
     data_file = os.path.join(data_dir, "okl_wavefields-20160711-055516_allsta.p")
     st = pickle.load(open(data_file, "rb"))
+    
+    #--- read inventory file
+    stationxml_file = os.path.join(data_dir, "station-okl.xml")
+    inventory = read_inventory(stationxml_file)
 
     #--- load rs_file that contains list of reference_secondaries combinations
     rs_file = os.path.join(data_dir, "reference_secondaries_combinations.json")
@@ -22,5 +32,7 @@ def read_example():
 
     #--- get earth model file path
     model_file = os.path.join(data_dir, "okl.tvel")
+    model_name = os.path.splitext(os.path.basename(model_file))[0]
+    taup_create.build_taup_model(model_file, verbose=False)
     
-    return st, rs_list, model_file
+    return event_dict, st, inventory, rs_list, model_name
